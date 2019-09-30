1b5edcb1bd1b6c6f97224633fc0b691eeeaf2ccc
Gabriel Bernardi <50278200+gabrielrbernardi@users.noreply.github.com>
2019-09-29 22:57:40 -0300

    Add files via upload

--- lab4.hs
+++ lab4.hs
@@ -19,6 +19,56 @@ n_tri x
     | otherwise = n_tri(x) + n_tri(x-1)
 -}
 
---QUESTAO 04
-fibo2 :: Int -> [(Int)]
-fibo2 y = [x | x <-y, fibo1 x]
+--QUESTAO 04 -- de acordo com a professora, nao precisa fazer
+-- fibo2 :: Int -> [(Int)]
+-- fibo2 y = [x | x <-y, fibo1 x]
+
+--QUESTAO 05
+fibo2 :: Int -> Int
+fibo2 1 = 1
+fibo2 2 = 1
+fibo2 n = fibo2(n-2)+fibo2(n-1)
+
+retornaFibo :: Int->[Int]
+retornaFibo x = [fibo x | x <- [1..50]]
+
+retornaFibo2 :: Int->[Int]
+retornaFibo2 x = [fibo2 x | x <- [1..50]]
+
+--R: acredito que o algoritmo que calcula fibonacci com casamento de padroes seja mais eficiente
+
+--QUESTAO 06
+potencia2 :: Int -> Int
+potencia2 x
+    | x == 0 = 1
+    | otherwise = 2^potencia2(x-1)
+
+--QUESTAO 07
+
+--QUESTAO 08
+
+--QUESTAO 09
+--usando guardas
+mdcg :: (Int,Int) -> Int
+mdcg (m,n)
+    | n == 0 = m
+    | otherwise = mdcg (n, (mod m n))
+--usando casamento de padroes
+mdc :: (Int,Int) -> Int
+mdc (m,0) = m
+mdc (m,n) = mdc (n, (mod m n))
+
+--QUESTAO 10
+--usando guardas
+binomialg :: (Int,Int) -> Int
+binomialg (n,0)  = 1
+binomialg (n,k)
+| k == 0 = 1
+| k == n = 1
+| otherwise = binomialg (n-1,k) + binomialg (n-1,k-1)
+
+--usando casamento de padroes
+binomial :: (Int,Int) -> Int
+binomial (n,0)  = 1
+binomial (n,k) = if (k == n) then 1
+                             else binomial (n-1,k) + binomial (n-1,k-1)
---
+++ trabalho1.hs
@@ -0,0 +1,58 @@
+--QUESTAO 01
+triangulo :: Int -> Int -> Int -> String
+triangulo x y z
+    | (x >= y+z) || (y >= x+z) || (z >= x+y) = "Nao eh triangulo"
+    | (x == y) && (y == z) && (z == x) = "Equilatero"
+    | (x == y) || (y == z) || (z == x) = "Isosceles"
+    | (x /= y) && (x /= z) && (y /= z) = "Escaleno"
+
+--QUESTAO 02
+type Raiz = (Float, Float)
+equacao :: Float -> Float -> Float -> Raiz
+equacao a b c 
+    | (delta a b c > 0) = ((abs (raiz1 (-b) (delta a b c) a)), (raiz2 (-b) (delta a b c) a)*(-1))
+    | (delta a b c == 0) = ((abs (raiz1 (-b) (delta a b c) a)), (raiz2 (-b) (delta a b c) a)*(-1)) 
+
+raiz2 :: Float -> Float -> Float -> Float
+raiz2 b d a = (((-b) + (sqrt d)) / (2*a))
+
+raiz1 :: Float -> Float -> Float -> Float
+raiz1 b d a = (((-b) - (sqrt d)) / (2*a))
+
+    -- | otherwise
+delta :: Float-> Float-> Float-> Float
+delta x y z = ((y**2)-(4*x*z))
+
+--QUESTAO 03
+passagem :: Float -> Int -> Float
+passagem valTot idade
+    | idade >= 60 = valTot*0.6
+    | idade < 2 = valTot*0.1
+    | idade <= 10 = valTot*0.5
+    | otherwise = valTot
+
+--QUESTAO 04
+-- A resolucao dessa questao estara em um arquivo anexado e que sera enviado junto
+
+--QUESTAO 05
+qtdNeg :: [Int] -> [Int]
+qtdNeg y = [x | x <- y, x < 0]
+
+--QUESTAO 06
+distancias :: [(Float,Float)] -> [Float]
+distancias [(a,b)] = [x | x <- [sqrt(a**2 + b**2)]]
+
+--QUESTAO 07
+ehPrimo :: Int -> Bool
+ehPrimo x = divisores x == [1,x]
+
+divisores :: Int -> [Int]
+divisores 1 = [1]
+divisores x = 1:[ y | y <- [2..(x `div` 2)], x `mod` y == 0] ++ [x]
+
+primos :: Int -> Int -> [Int]
+primos x y = [x | x <- [x..y], ehPrimo x]
+
+--QUESTAO 08
+-- notasTroco :: Int -> [[Int]]
+notasTroco y = [x | x <- [1..45]]
